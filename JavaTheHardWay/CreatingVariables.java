0b8a153eab6b41d462d974495240de621db6be6e
Matt <mra286@humboldt.edu>
2019-10-25 12:52:23 -0700

    Complete exercise 5 + 6

---
+++ JavaTheHardWay/CreatingVariables.java
@@ -0,0 +1,36 @@
+public class CreatingVariables {
+  public static void main( String[] args ) {
+    int x, y, age, days;
+    double seconds, e, checking, time;
+    String firstName, lastName, title, myName, language;
+
+    x = 10;
+    y = 400;
+    age = 39;
+    seconds = 4.71;
+    e = 2.71828182845904523536;
+    checking = 1.89;
+
+    firstName = "Graham";
+    lastName = "Mitchell";
+    title = "Mr.";
+
+    days = 2;
+    time = 6.5;
+    myName = "Matt Aldrete";
+    language = "Java";
+
+
+
+
+    System.out.println( "The variable x contains " + x );
+    System.out.println( "The value " + y + " is stored in the variable y." );
+    System.out.println( "The experiment took " + seconds + " seconds." );
+    System.out.println( "A favorite irrational # is Euler's number: " + e );
+    System.out.println( "Hopefully you have more that $" + checking + "!" );
+    System.out.println( "My name's " + title + " " + firstName + lastName );
+    System.out.println( "My name is " + myName + " and I am learning " + language);
+    System.out.println( "So far I have been coding for " + days + " days" + " and" + " " + time + " hours.");
+
+  }
+}
