d9080b6217d5bff0229d9ce49b0a694fc383f530
Matt <mra286@humboldt.edu>
2019-10-25 13:36:12 -0700

    Completed Objective 2 Labs 1-5

---
+++ labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Matt Aldrete";
+    int age = 28;
+    String state = "California";
+
+    System.out.println( "Name: " + name + " " + "Age: " + age + " " + "State: " + state);
+  }
+}
---
+++ labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 22.5;
+    num2 = 6.8;
+    num3 = 100.99;
+
+    average = ((num1+num2+num3)/3);
+    System.out.println(average);
+  }
+}
