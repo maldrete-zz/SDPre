5cfa9ae42f40aca791bd2d58486233e70ad7f115
Matt <mra286@humboldt.edu>
2019-10-25 02:07:12 -0700

    Completed Exercise 1 and 2

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,7 +1,6 @@
-// Exercise 1
 public class FirstProg {
   public static void main( String[] args ) {
-      System.out.println( "I am determined to learn how to code." );
-      System.out.println( "Today's date is" );
+      System.out.println( "I Matt am determined to learn how to code." );
+      System.out.println( "Today's date is October 25, 2019" );
     }
 }
