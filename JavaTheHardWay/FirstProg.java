5d085ce69c3e0231edf6ce06e1021d4228551bfd
Matt <mra286@humboldt.edu>
2019-10-25 01:33:05 -0700

    added labs and exercise 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,7 @@
+// Exercise 1
+public class FirstProg {
+  public static void main( String[] args ) {
+      System.out.println( "I am determined to learn how to code." );
+      System.out.println( "Today's date is" );
+    }
+}
